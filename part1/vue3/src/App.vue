2516b5b0cf602129f2d8d677e595fd57ff6338e3
kenu <kenu.heo@gmail.com>
2022-03-31 09:18:47 +0900

    react

--- part1/vue3/src/App.vue
+++ part1/vue3/src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>
+    <img alt="React + API Server 개발과 배포" src="./assets/inflearn-react-api.png" />
+    <p>
+      <a
+        href="https://www.inflearn.com/course/%EB%A6%AC%EC%95%A1%ED%8A%B8-api-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8"
+        target="blank"
+      >React + API Server 프로젝트 개발과 배포</a>
+    </p>
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  </div>
 </template>
 
 <script>
